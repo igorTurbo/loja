58e6fb4c29c3a0a18a44820664ac121b88a40939
igorTurbo <114207329+igorTurbo@users.noreply.github.com>
2022-09-23 01:32:39 -0300

    Add files via upload

---
+++ hydrogen.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'lojaderoupatest.myshopify.com',
+    storefrontToken: '571073b70161d37428f45d6adc5ab36b',
+    storefrontApiVersion: '2022-07',
+  },
+});
